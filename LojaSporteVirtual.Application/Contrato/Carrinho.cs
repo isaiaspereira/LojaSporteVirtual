52e38b530abfa96b49d2623dd570c56f8840b929
isaias pereira <isaias.pereira0003@hotmail.com>
2018-07-19 20:13:01 -0400

    Concluindo  os contratos do carrinho para o processo de compra.

---
+++ LojaSporteVirtual.Application/Contrato/Carrinho.cs
@@ -0,0 +1,49 @@
+﻿using LojaSporteVirtual.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaSporteVirtual.Application.Contrato
+{
+    public class Carrinho
+    {
+        private readonly List<ItemCarrinho> _itemCarrinho = new List<ItemCarrinho>();
+        public void AdicionarItem(Produto produto, int quantidade)
+        {
+            ItemCarrinho item = _itemCarrinho.FirstOrDefault(p => p.produto.ProdutoId == produto.ProdutoId);
+
+            if (item == null)
+            {
+                _itemCarrinho.Add(new ItemCarrinho
+                {
+                    produto = produto,
+                    Quantidade = quantidade
+                });
+            }
+            else
+                item.Quantidade += quantidade;
+        }
+
+        public void RemoverItem(Produto produto)
+        {
+            _itemCarrinho.RemoveAll(R => R.produto.ProdutoId == produto.ProdutoId);
+        }
+
+        public decimal ObterValorTotal()
+        {
+            return _itemCarrinho.Sum(s => s.produto.Preco * s.Quantidade);
+        }
+
+        public void LimparCarrinho()
+        {
+            _itemCarrinho.Clear();
+        }
+
+        public IEnumerable<ItemCarrinho> ItensDoCarrinho
+        {
+            get { return _itemCarrinho.ToList(); }
+        }
+    }
+}
---
+++ LojaSporteVirtual.Domain/Entity/ItemCarrinho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaSporteVirtual.Domain.Entity
+{
+   public class ItemCarrinho
+    {
+        public Produto produto { get; set; }
+
+        public int Quantidade { get; set; }
+    }
+}
